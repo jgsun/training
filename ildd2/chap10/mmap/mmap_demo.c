0ae130545132680253b060151473169dc1943fc7
jgsun <jianguo_sun@hotmail.com>
2017-10-24 19:18:42 +0800

    ildd2: mmap: add device_create for mmap_demp
    let kernel create device node when loading module.

--- ildd2/chap10/mmap/mmap_demo.c
+++ ildd2/chap10/mmap/mmap_demo.c
@@ -11,13 +11,23 @@
 #include <linux/mm_types.h>
 #include <linux/mm.h>
 #include <linux/highmem.h>
+#include <linux/errno.h>        /* error codes */
 
 #define KSTR_DEF    "Hello world from kernel virtual space"
-    
-static struct cdev *pcdev;
+
 static dev_t ndev;
 static struct page *pg;
 static struct timer_list timer;
+
+static struct class mmap_demo_class = {
+	.name = "mmap_demo",
+};
+
+struct mmap_demo_dev {
+	struct device *dev;
+	struct cdev *pcdev;
+};
+static struct mmap_demo_dev mmap_demo_dev;
 
 static void timer_func(unsigned long data)
 {
@@ -32,7 +42,7 @@ static int demo_open(struct inode *inode, struct file *filp)
 }
 
 static int demo_release(struct inode *inode, struct file *filp)
-{   
+{
     return 0;
 }
 
@@ -41,11 +51,10 @@ static int demo_mmap(struct file *filp, struct vm_area_struct *vma)
     int err = 0;
     unsigned long start = vma->vm_start;
     unsigned long size = vma->vm_end - vma->vm_start;
-  
+
     err = remap_pfn_range(vma, start, vma->vm_pgoff, size, vma->vm_page_prot);
     return err;
 }
-
 
 static struct file_operations mmap_fops =
 {   .owner = THIS_MODULE,
@@ -53,8 +62,6 @@ static struct file_operations mmap_fops =
     .release = demo_release,
     .mmap = demo_mmap,
 };
-
-
 
 static int demo_map_init(void)
 {
@@ -71,20 +78,34 @@ static int demo_map_init(void)
     strcpy(kstr, KSTR_DEF);
     printk("kpa = 0x%lx, kernel string = %s\n", page_to_phys(pg), kstr);
 
-    pcdev = cdev_alloc();
-    if(!pcdev){
-        err = -ENOMEM; 
-        goto cdev_err;  
+    mmap_demo_dev.pcdev = cdev_alloc();
+    if(!mmap_demo_dev.pcdev){
+        err = -ENOMEM;
+        goto cdev_err;
     }
-    cdev_init(pcdev, &mmap_fops);
+    cdev_init(mmap_demo_dev.pcdev, &mmap_fops);
     err = alloc_chrdev_region(&ndev, 0, 1, "mmap_dev");
     if(err)
         goto region_err;
     printk("major = %d, minor = %d\n", MAJOR(ndev), MINOR(ndev));
-    pcdev->owner = THIS_MODULE;
-    err = cdev_add(pcdev, ndev, 1);
+    mmap_demo_dev.pcdev->owner = THIS_MODULE;
+    err = cdev_add(mmap_demo_dev.pcdev, ndev, 1);
     if(err)
         goto add_err;
+
+    err = class_register(&mmap_demo_class);
+    if (err) {
+        printk(KERN_ERR "class_register failed for scull\n");
+        goto add_err;
+    }
+    mmap_demo_dev.dev = device_create(&mmap_demo_class, NULL,
+		ndev, &mmap_demo_dev,
+		"mmap_demo");
+    if (IS_ERR(mmap_demo_dev.dev)) {
+        printk(KERN_ERR "mmap_demo: device register failed\n");
+        err = PTR_ERR(mmap_demo_dev.dev);
+        goto add_err;
+    }
 
     init_timer(&timer);
     timer.function = timer_func;
@@ -92,27 +113,27 @@ static int demo_map_init(void)
     timer.expires = jiffies + HZ*10;
     add_timer(&timer);
     goto done;
-    
+
 add_err:
     unregister_chrdev_region(ndev, 1);
 region_err:
-    cdev_del(pcdev);
+    cdev_del(mmap_demo_dev.pcdev);
 cdev_err:
     kunmap(pg);
     ClearPageReserved(pg);
-    __free_pages(pg, 0); 
-done:         
+    __free_pages(pg, 0);
+done:
     return err;
 }
 
 static void demo_map_exit(void)
 {
     del_timer_sync(&timer);
-    cdev_del(pcdev); 
+    cdev_del(mmap_demo_dev.pcdev);
     unregister_chrdev_region(ndev, 1);
-    kunmap(pg); 
-    ClearPageReserved(pg); 
-    __free_pages(pg, 0); 
+    kunmap(pg);
+    ClearPageReserved(pg);
+    __free_pages(pg, 0);
 }
 
 module_init(demo_map_init);
